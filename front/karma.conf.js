d055892dc252a24299ab2e9bdf3ecc5b3f49f4cf
Jules Grelet <jules.grelet@live.fr>
2024-06-06 01:20:48 +0200

    Update front & back workflows and config files

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -26,18 +26,31 @@ module.exports = function (config) {
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/bobapp'),
+      reports: ['html', 'text-summary'],
       subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-      ]
+      ],
+      /*
+      Will be used soon to improve test validation.
+      CI step of test will only be validated if rates (80%) are all reached.
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
+      */
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
